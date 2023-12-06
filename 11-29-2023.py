9c972e4a9992fe4ab0fbe9afca7f0de33d0bd9b0
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2023-12-06 17:45:29 +0800

    Create 11-29-2023.py

---
+++ 11-29-2023.py
@@ -0,0 +1,23 @@
+# def check_unique_titles(book_titles: list[str]):
+#     return len(set(book_titles)) == len(book_titles)
+#
+#
+# titles = ['I Was Told There\'d Be Cake', 'Pale Fire', 'Dorothy Must Die', 'Full Dark, No Stars',
+#           'Full Dark, No Stars']
+#
+# print(check_unique_titles(titles))
+
+# x - new and empty file
+# w - new file that is writable
+
+file = open('it-3-ya-2.txt', 'r+')
+
+# r - read only
+# r+ - read and write
+# w - write only
+# w+ - write and read
+# a - append
+# a+ - append and read
+
+
+print(file.readline())
